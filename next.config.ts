7aae8110f120fac5fec8096dc8d543df0e607ac7
Abid Hossain <abidhossain1080@gmail.com>
2025-04-20 14:09:07 +0200

    Configure Next.js for static export and add GitHub Pages deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
 
 export default nextConfig;
